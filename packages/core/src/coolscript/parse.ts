806882f51456d5dd809019f8e1189a0217983933
Gabriel Anderson <gabe4k@gmail.com>
2022-01-02 21:18:51 -0800

    setup eslint and prettier

--- packages/core/src/coolscript/parse.ts
+++ packages/core/src/coolscript/parse.ts
@@ -1,5 +1,5 @@
 import { Syntax } from './syntax'
 
-export function parse(_src: string): Syntax {
-  return { tag: 'Expr' }
+export function parse(src: string): Syntax {
+  return { tag: src }
 }
--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,5 +1,5 @@
 import { parse } from 'language-coolscript'
 
 export function App() {
-  return <h1>{parse('hello world').tag}</h1>;
+  return <h1>{parse('hello world').tag}</h1>
 }
