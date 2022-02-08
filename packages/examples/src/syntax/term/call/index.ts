40eb76d514472a24d860d2eafa8416564f7680ff
Gabriel Anderson <gabe4k@gmail.com>
2022-02-08 09:09:36 -0800

    added more syntax examples

---
+++ packages/examples/src/syntax/term/call/index.ts
@@ -0,0 +1,3 @@
+import call1 from './call1.cs'
+
+export const callExamples: { [key: string]: string } = { call1 }
--- packages/examples/src/syntax/term/index.ts
+++ packages/examples/src/syntax/term/index.ts
@@ -1,9 +1,11 @@
 import { valueExamples } from './value'
 import { letExamples } from './let'
 import { lambdaExamples } from './lambda'
+import { callExamples } from './call'
 
 export const termExamples: { [key: string]: string } = {
   ...valueExamples,
   ...letExamples,
-  ...lambdaExamples
+  ...lambdaExamples,
+  ...callExamples
 }
